b3879bc39f273b2ec533d57b9c86f2570554d2b9
Jhone-cmd <jhone.1941@gmail.com>
2025-07-15 17:48:44 -0300

    chore: created the audio transcription service using Gemini's AI

--- src/routes/upload-audio.ts
+++ src/routes/upload-audio.ts
@@ -1,16 +1,14 @@
 import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod';
 import z from 'zod/v4';
+import { transcribeAudio } from '../services/gemini.ts';
 
 export const uploadAudio: FastifyPluginCallbackZod = (app) => {
   app.post(
-    '/rooms/:roomId/questions',
+    '/rooms/:roomId/audio',
     {
       schema: {
         params: z.object({
           roomId: z.string(),
-        }),
-        body: z.object({
-          question: z.string().min(3),
         }),
       },
     },
@@ -26,7 +24,14 @@ export const uploadAudio: FastifyPluginCallbackZod = (app) => {
         const audioBuffer = await audio.toBuffer();
         const audioAsBase64 = audioBuffer.toString('base64');
 
-        return audioAsBase64;
+        const transcription = await transcribeAudio(
+          audioAsBase64,
+          audio.mimetype
+        );
+
+        console.log(transcription);
+
+        return 'ok';
       } catch (error) {
         console.warn(error);
       }
