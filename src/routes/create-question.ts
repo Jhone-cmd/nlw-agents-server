3947fb02889894a5d7f9fa150218265e97101304
Jhone-cmd <jhone.1941@gmail.com>
2025-07-16 15:53:40 -0300

    chore: The change was made to the createQuestion route, generating the question embeddings with the audio embeddings

--- src/routes/create-question.ts
+++ src/routes/create-question.ts
@@ -1,8 +1,11 @@
+import { and, eq, sql } from 'drizzle-orm';
+import { vector } from 'drizzle-orm/pg-core';
 import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod';
 import z from 'zod/v4';
 import { db } from '../db/connection.ts';
 import { schema } from '../db/schema/index.ts';
 import { FailedCreate } from '../errors/failed-create.ts';
+import { generateEmbeddings } from '../services/gemini.ts';
 
 export const createQuestion: FastifyPluginCallbackZod = (app) => {
   app.post(
@@ -21,23 +24,46 @@ export const createQuestion: FastifyPluginCallbackZod = (app) => {
       try {
         const { roomId } = request.params;
         const { question } = request.body;
-        const { questions } = schema;
+        const { questions, audioChunks } = schema;
 
-        const result = await db
-          .insert(questions)
-          .values({
-            question,
-            roomId,
+        const embeddings = await generateEmbeddings(question);
+
+        const embeddingsAsString = `[${embeddings.join(',')}]`;
+
+        const chunks = await db
+          .select({
+            id: audioChunks.id,
+            transcription: audioChunks.transcription,
+            similarity: sql<number>`1 - (${audioChunks.embeddings} <=> ${embeddingsAsString}::vector)`,
           })
-          .returning();
+          .from(audioChunks)
+          .where(
+            and(
+              eq(audioChunks.roomId, roomId),
+              sql`1 - (${audioChunks.embeddings} <=> ${embeddingsAsString}::vector) > 0.8`
+            )
+          )
+          .orderBy(
+            sql`1 - (${audioChunks.embeddings} <=> ${embeddingsAsString}::vector)`
+          )
+          .limit(5);
 
-        const insertedQuestion = result[0];
+        // const result = await db
+        //   .insert(questions)
+        //   .values({
+        //     question,
+        //     roomId,
+        //   })
+        //   .returning();
 
-        if (!insertedQuestion) {
-          throw new FailedCreate('question');
-        }
+        // const insertedQuestion = result[0];
 
-        return reply.status(201).send({ questionId: insertedQuestion.id });
+        // if (!insertedQuestion) {
+        //   throw new FailedCreate('question');
+        // }
+
+        // return reply.status(201).send({ questionId: insertedQuestion.id });
+        return chunks;
       } catch (error) {
         if (error instanceof FailedCreate) {
           return reply.status(400).send({ message: error.message });
