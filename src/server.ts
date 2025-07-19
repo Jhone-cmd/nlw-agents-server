849006dc20e7dc920fc0e1b7dde7cdb9abffbb4b
Jhone-cmd <jhone.1941@gmail.com>
2025-07-19 15:52:28 -0300

    chore: configuring server for deploy

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from './app.ts';
 import { env } from './env.ts';
 
-app.listen({ port: env.PORT }).then(() => {
+app.listen({ host: '0.0.0.0', port: env.PORT }).then(() => {
   console.log('Server Running!');
 });
